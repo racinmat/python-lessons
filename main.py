e910050d3a28bde4f9191ee9d8126cb9b7617252
racinmat <racinmat@fel.cvut.cz>
2017-08-14 19:55:36 +0200

    Added description.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 def my_function(array):
 #     todo: implement
+# this function should multiply even numbers by two and divide odd numbers by two, and return array with modified numbers
     return []
 
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
