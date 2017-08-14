9f6529cdb5447c5602fa3cab10a9e0f04be40a2e
racinmat <racinmat@fel.cvut.cz>
2017-08-14 23:52:49 +0200

    Added more snippets.

---
+++ functions.py
@@ -0,0 +1,9 @@
+def multiply_by_three(arr):
+    for i, j in enumerate(arr):
+        arr[i] = j * 3
+    return arr
+
+if __name__ == '__main__':
+    print("nazdar, tady se importuje")
+
+print(__name__)
--- main.py
+++ main.py
@@ -1,19 +1,64 @@
 # my python file
+# from math import sin
+# from functions import multiply_by_three
+
 
 def my_function(array):
 #     todo: implement
 # this function should multiply even numbers by two and divide odd numbers by two, and return array with modified numbers
     return []
 
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+def my_lambda(x):
+    return x * 2
 
-print(my_function(array))
+def is_even(x):
+    return x % 2 == 0
 
+if __name__ == '__main__':
+    my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    #
+    # print(my_function(array))
+    #
+    #
+    # for i in array:
+    #     print("vypisuju")
+    #     print(i)
+    #
+    # var = 3
+    # if var / 3 == 1:
+    #     print('je to 3')
+    #
+    #
+    # new_arr = []
+    # for i, j in enumerate(array):
+    #     k = sin(0.3)
+    #     new_arr.append(k)
+    #
+    # print(multiply_by_three(array))
+    # val1 = 5
+    # val2 = 4.6
+    # print("hodnoty jsou " + str(val1) + " a " + str(val2))
+    # print("hodnoty jsou {:d} a {:0.2f}".format(val1, val2))
 
-for i in array:
-    print("vypisuju")
-    print(i)
+    # i = 0
+    # while True:
+    #     x = int(input("zadej změnu\n"))
+    #     i = (i + x) % 4
+    #     print(i)
 
-var = 3
-if var / 3 == 1:
-    print('je to 3')
+    # my_lambda = lambda x: x * 2
+    # is_even = lambda x: x % 2 == 0
+    # print(my_lambda(5))
+    # print(is_even(5))
+    my_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # print(map(lambda x: x - 6, array))
+    # print(list(map(lambda x: x - 6, array)))
+    # print(list(map(lambda x: x > 4, array)))
+    # print(list(filter(lambda x: not x % 2 == 0, array)))
+
+    # print([x / 2 for x in my_array])
+    print([x for x in range(0, 15)])
+    print([x * 2 for x in range(0, 15)])
+    print([x for x in range(0, 15) if x < 10])
+    print([x * 2 for x in range(0, 15) if x < 10])
+
